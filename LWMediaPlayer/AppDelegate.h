57eccd6c2ae6c38a55af8e5cc4249cb4cfa6692a
Alienchang <1217493217@qq.com>
2015-03-31 17:20:06 +0800

    second

---
+++ LWMediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWMediaPlayer
+//
+//  Created by 刘畅 on 15/3/31.
+//  Copyright (c) 2015年 刘畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWMediaPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LWMediaPlayer
+//
+//  Created by 刘畅 on 15/3/31.
+//  Copyright (c) 2015年 刘畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
