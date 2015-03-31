5c81150bca653eefe8da09dcb8396e85c6857be3
wimw <whoiamwhere@gmail.com>
2015-03-31 18:16:27 +0800

    asd

---
+++ LWMediaPlayer/LWMediaPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LWMediaPlayer
+//
+//  Created by 刘畅 on 15/3/31.
+//  Copyright (c) 2015年 刘畅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
